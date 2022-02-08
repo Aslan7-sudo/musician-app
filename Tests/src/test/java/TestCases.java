44f6a84c40f8953145a61a15d450223320b57e28
Aslan7-sudo <74963422+Aslan7-sudo@users.noreply.github.com>
2022-02-08 19:21:00 +0530

    Update TestCases.java

--- Tests/src/test/java/TestCases.java
+++ Tests/src/test/java/TestCases.java
@@ -3,6 +3,6 @@ import org.testng.annotations.Test;
 public class TestCases {
     @Test
     void test1(){
-        System.out.println("I am inside test1 ");
+        Systemout.println("I am inside test1 ");
     }
 }
